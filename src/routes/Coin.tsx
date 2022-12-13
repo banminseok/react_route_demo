dc038df86118bcb19dcf25970175bbb93dadff0b
ban <halfmin@gmail.com>
2022-12-13 13:46:55 +0900

    init

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+function Coin() {
+  return (
+    <Container>
+      <h1>Coin</h1>
+    </Container>
+  );
+}
+
+export default Coin;
